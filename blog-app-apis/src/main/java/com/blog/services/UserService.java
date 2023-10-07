2ed028081e65af73e3e9605e02e753991d997ecb
Vallabh-K <vallabhvalla@gmail.com>
2023-10-07 10:58:37 +0530

    fix

---
+++ blog-app-apis/src/main/java/com/blog/services/UserService.java
@@ -0,0 +1,21 @@
+package com.blog.services;
+
+import java.util.List;
+
+import com.blog.entities.User;
+import com.blog.payloads.UserDto;
+
+public interface UserService {
+
+	UserDto createUser(UserDto user);
+	
+	UserDto updateUser(UserDto user,Integer userId);
+	
+	UserDto getUserById(Integer userId);
+	
+	List<UserDto> getAllUser();
+	
+	void deleteUser(Integer userId);
+	
+	
+}
