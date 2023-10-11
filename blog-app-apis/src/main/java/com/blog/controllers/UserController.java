f323bff04033f6031564cb076f28e309681ab0fa
Vallabh-K <vallabhvalla@gmail.com>
2023-10-11 23:05:07 +0530

    added input validation

--- blog-app-apis/src/main/java/com/blog/controllers/UserController.java
+++ blog-app-apis/src/main/java/com/blog/controllers/UserController.java
@@ -18,6 +18,7 @@ import com.blog.payloads.ApiResponse;
 import com.blog.payloads.UserDto;
 import com.blog.services.UserService;
 
+import jakarta.validation.Valid;
 import lombok.experimental.PackagePrivate;
 
 @RestController
@@ -32,7 +33,7 @@ public class UserController {
 	
 	//POST - create user
 	@PostMapping("/")
-	public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto){
+	public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserDto userDto){
 		UserDto createUserDto = this.userService.createUser(userDto);
 		return new ResponseEntity<>(createUserDto, HttpStatus.CREATED);
 	}
@@ -40,7 +41,7 @@ public class UserController {
 	// PUT - update user
 	
 	@PutMapping("/{userId}")
-	public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto, @PathVariable("userId") Integer uid){
+	public ResponseEntity<UserDto> updateUser(@Valid @RequestBody UserDto userDto, @PathVariable("userId") Integer uid){
 		
 		UserDto updatedUser = this.userService.updateUser(userDto, uid);
 		return ResponseEntity.ok(updatedUser);
--- blog-app-apis/src/main/java/com/blog/payloads/UserDto.java
+++ blog-app-apis/src/main/java/com/blog/payloads/UserDto.java
@@ -1,5 +1,10 @@
 package com.blog.payloads;
 
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,9 +15,19 @@ import lombok.Setter;
 public class UserDto {
 	
 	private int id;
+	
+	@NotEmpty
+	@Size(min = 4, message = "User name must be min 4 characters !!")
 	private String name;
+	
+	@Email(message = "Emai address is not valid !!")
 	private String email;
+	
+	@NotEmpty
+	@Size(min = 3, max = 10, message = "Password must be min of 3 chars and max of 10 chars !!")
 	private String password;
+	
+	@NotEmpty(message = "Write something in about")
 	private String about;
 
 
